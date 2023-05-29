1517779558ec13b3d9e1e42d4f6099b021ed077c
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-05-29 21:26:17 +0600

    [Done] ESlint, prettier, husky setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,13 @@
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from 'dotenv'
+import path from 'path'
 
 /* This code is using the `dotenv` package to load environment variables from a `.env` file located in
 the root directory of the project. process.cwd() means the root directory*/
 dotenv.config({
-    path: path.join(process.cwd(), '.env')
+  path: path.join(process.cwd(), '.env'),
 })
 
 export default {
-    port: process.env.PORT || 8000,
-    database_string: process.env.DATABASE_STRING,
-};
+  port: process.env.PORT || 8000,
+  database_string: process.env.DATABASE_STRING,
+}
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function databaseConnection() {
-    try {
-        await mongoose.connect(config.database_string as string);
-        console.log('Database connected successfully');
+  try {
+    await mongoose.connect(config.database_string as any)
+    console.log('Database connected successfully')
 
-        app.listen(config.port, () => {
-            console.log(`University server is listening on port ${config.port}`)
-          })
-    } catch (error) {
-        console.log('Error while connecting database: ', error);
-    }
+    app.listen(config.port, () => {
+      console.log(`University server is listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('Error while connecting database: ', error)
+  }
 }
 
-databaseConnection();
+databaseConnection()
